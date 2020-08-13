c514618c1a223a98598f78edf3c2c9cc71f387fd
Fabricio Elizondo Fernández <fabelifer@gmail.com>
2020-08-13 10:41:06 -0600

    gitflow-feature-stash: reporteInventario

--- Web/src/app/app.routes.ts
+++ Web/src/app/app.routes.ts
@@ -3,12 +3,18 @@ import {HomeAdminComponent} from './componentes/administrador/home-admin/home-ad
 import {CalendarioComponent  } from './componentes/compartidos/calendario/calendario.component';
 import { HomeOperadorComponent } from './componentes/operador/home-operador/home-operador.component';
 import { ReservaLabComponent } from './componentes/operador/reserva-lab/reserva-lab.component';
+import {ReporteAveriaComponent} from './componentes/operador/reporte-averia/reporte-averia.component';
+import {ReportesGeneradosAveriasComponent} from './componentes/operador/reportes-generados-averias/reportes-generados-averias.component';
+
+
 
 const APP_ROUTES: Routes = [
     {path: 'homeOperador',component:HomeOperadorComponent},
     {path: 'homeAdmin', component: HomeAdminComponent},
     {path: 'calendario',component: CalendarioComponent},
     {path: 'reservaLaboratorio', component: ReservaLabComponent},
+    {path: 'reporteAveria', component: ReporteAveriaComponent},
+    {path: 'reportesGeneradosAverias',component:ReportesGeneradosAveriasComponent},
     {path: '**', pathMatch: 'full', redirectTo: 'homeOperador'}
 ];
 
