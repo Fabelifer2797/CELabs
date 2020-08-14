5fb5008f15090497183e684c9bcb13a863fb78a7
Fabricio Elizondo Fernández <fabelifer@gmail.com>
2020-08-14 05:20:52 -0600

    gitflow-feature-stash: login

---
+++ Web/src/app/componentes/compartidos/crear-cuenta/crear-cuenta.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CrearCuentaComponent } from './crear-cuenta.component';
+
+describe('CrearCuentaComponent', () => {
+  let component: CrearCuentaComponent;
+  let fixture: ComponentFixture<CrearCuentaComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CrearCuentaComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CrearCuentaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
